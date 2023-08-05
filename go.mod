3f4504a8ee6b437e39fbdf58d0c252cb06ff94ad
Stefan Cimander <stefan.cimander@me.com>
2023-08-05 13:49:53 +0200

    Add customer mock data and implement GET endpoints

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/stefan-cimander/crm-backend
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+type Customer struct {
+	Id        int    `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Role      string `json:"role,omitempty"`
+	Company   string `json:"company,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+	Contacted bool   `json:"contacted,omitempty"`
+}
+
+var customers = []Customer{
+	{Id: 1, Name: "Tim Cook", Role: "CEO", Company: "Apple", Email: "tim.cook@apple.com", Phone: "+1 123 456 789", Contacted: true},
+	{Id: 2, Name: "Larry Page", Role: "Founder", Company: "Google", Email: "larry.page@google.com", Phone: "+1 234 567 890", Contacted: true},
+	{Id: 3, Name: "Elon Musk", Role: "Founder & CEO", Company: "Tesla", Email: "elon.musk@tesla.com", Phone: "+1 345 678 901", Contacted: false},
+}
+
 func getCustomers(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get customers...")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(customers)
 }
 
 func getCustomer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get customer...")
+	w.Header().Set("Content-Type", "application/json")
+
+	idParam := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParam)
+
+	for _, customer := range customers {
+		if customer.Id == id {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(customer)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func addCustomer(w http.ResponseWriter, r *http.Request) {
